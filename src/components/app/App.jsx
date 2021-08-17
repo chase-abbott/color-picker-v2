519aa1926c9a9966bcaff9d9a21fd5698589947e
Chase <cabbott93@gmail.com>
2021-08-17 11:03:45 -0700

    refactor with action functions

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,21 +1,33 @@
 /* eslint-disable max-len */
 import React from 'react';
 import { useDispatch, useSelector } from '../../state/RecordProvider';
-import { RECORD, UNDO, REDO } from '../../state/actions';
+import { undoAction, redoAction, recordAction } from '../../state/actions';
 
 
 function App() {
   const dispatch = useDispatch();
   const selector = useSelector();
 
+  const undo = () => (
+    dispatch(undoAction())
+  );
+
+  const redo = () => (
+    dispatch(redoAction())
+  );
+
+  const record = value => (
+    dispatch(recordAction(value))
+  );
+
   return (
     <>
-      <button onClick={() => dispatch({ type: UNDO })}>undo</button>
-      <button onClick={() => dispatch({ type: REDO })}>redo</button>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
       <input
         type="color"
         value={selector.current}
-        onChange={({ target }) => dispatch({ type: RECORD, payload: target.value })}
+        onChange={({ target }) => record(target.value)}
         data-testid="input"
       />
       <div
