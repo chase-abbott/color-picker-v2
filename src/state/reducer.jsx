f9778c1075ff15fe0824d02d75fa2686adb7d10d
Chase <cabbott93@gmail.com>
2021-08-16 17:53:24 -0700

    remove state spread

--- src/state/reducer.jsx
+++ src/state/reducer.jsx
@@ -11,14 +11,12 @@ const reducer = (state, action) => {
   switch(action.type){
     case UNDO:
       return { 
-        ...state,
         after: [state.current, ...state.after],
         current: state.before[state.before.length - 1],
         before: state.before.slice(0, -1),
       };
     case REDO:
       return {
-        ...state,
         before: [...state.before, state.current],
         current: state.after[0],
         after: state.after.slice(1)
